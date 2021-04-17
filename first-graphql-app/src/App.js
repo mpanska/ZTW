4b379fab3d323ae74c4ed7a7f0303a44a541593b
Małgorzata Pańska <47063770+mpanska@users.noreply.github.com>
2021-04-17 14:00:08 +0200

    base instruction

---
+++ first-graphql-app/src/App.js
@@ -0,0 +1,69 @@
+const axios = require("axios") 
+
+const { GraphQLServer } = require('graphql-yoga');
+
+
+const resolvers = {
+    Query: {
+        users: async () => getRestUsersList(),
+        todos: async () => getRestTodoList(),
+        todo: (parent, args, context, info) => todoById(parent, args, context, info),
+        user: (parent, args, context, info) => userById(parent, args, context, info), 
+    },
+    User:{
+        todos: (parent, args, context, info) => {
+            return todosList.filter(t => t.user_id == parent.id);
+        }
+    },
+    ToDoItem:{
+        user: (parent, args, context, info) => {
+            return usersList.find(u => u.id == parent.user_id);
+        }
+    },
+} 
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+});
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
+
+function todoById(parent, args, context, info){
+    return todosList.find(t => t.id == args.id);
+}
+
+function userById(parent, args, context, info){
+    return usersList.find(u => u.id == args.id);
+} 
+
+    async function getRestUsersList(){
+        try {
+            const users = await axios.get("https://jsonplaceholder.typicode.com/users")
+            console.log(users);
+            return users.data.map(({ id, name, email, username }) => ({
+                id: id,
+                name: name,
+                email: email,
+                login: username,
+        }))
+        } catch (error) {
+            throw error
+        }
+   } 
+
+
+async function getRestTodoList(){
+    try {
+        const todo = await axios.get("https://jsonplaceholder.typicode.com/todos")
+        console.log(todo);
+        return todo.data.map(({ id, title, completed, user_id }) => ({
+            id: id,
+            title: title,
+            completed: completed,
+            user_id: user_id,
+    }))
+    } catch (error) {
+        throw error
+    }
+} 
